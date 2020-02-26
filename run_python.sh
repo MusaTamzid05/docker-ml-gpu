950431d744b82f34d03d40a0742f18572fc7bee4
MusaTamzid05 <tamzid101@gmail.com>
2020-02-27 01:03:22 +0600

    shell  made dynamic.

--- run_python.sh
+++ run_python.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
- docker run --gpus all --rm  -it --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp  musa101/ml-gpu python main.py
+if [ $# != 1 ]
+then
+    echo "Usage: ./run_python.sh main.py"
+    exit 1
+fi
+
+ docker run --gpus all --rm  -it --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp  musa101/ml-gpu python $1
